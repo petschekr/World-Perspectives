e5de2f079e48f21f1daeb10ddbf4a794605e487c
Ryan Petschek <petschekr@gmail.com>
2015-03-24 21:09:45 -0400

    Set up Socket.io on client and server

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ var r = require("rethinkdb");
 var DBConnection = undefined;
 function intializeDatabase() {
 	r.connect({"db": "wpp", "timeout": 20})
-		.then(function(connection) {
+		.then(function (connection) {
 			DBConnection = connection;
 			console.log("Connected to RethinkDB successfully");
 		})
-		.error(function(err) {
+		.error(function (err) {
 			// Can't connect to the database
 			// Fatal error
 			throw err;
@@ -34,6 +34,7 @@ var compress = require("compression");
 var cookieParser = require("cookie-parser");
 
 var app = express();
+var server = require("http").Server(app);
 
 app.use(compress());
 app.use(responseTime());
@@ -51,40 +52,59 @@ app.use("/components", serveStatic("components"));
 app.use("/css", serveStatic("css"));
 app.use("/img", serveStatic("img"));
 
-app.route("/").get(function(request, response) {
-	fs.readFileAsync("signup.html", {"encoding": "utf8"})
-		.then(function(html) {
+// Set up the Socket.io server
+var io = require("socket.io")(server);
+
+app.route("/register").get(function (request, response) {
+	fs.readFileAsync("invalidcode.html", {"encoding": "utf8"})
+		.then(function (html) {
 			response.send(html);
 		});
 });
-app.route("/sessions/panels").get(function(request, response) {
-	r.table("panels").orderBy("name").run(DBConnection).then(function(data) {
+app.route("/register/:code").get(function (request, response) {
+	var code = request.params.code.toString();
+
+	fs.readFileAsync("signup.html", {"encoding": "utf8"})
+		.then(function (html) {
+			response.send(html);
+		});
+});
+app.route("/sessions/panels").get(function (request, response) {
+	r.table("panels").orderBy("name").run(DBConnection).then(function (data) {
 		response.send(data);
 	});
 });
-app.route("/sessions/wpp").get(function(request, response) {
-	r.table("sessions").filter({"type": "wpp"}).orderBy("name").run(DBConnection).then(function(data) {
+app.route("/sessions/wpp").get(function (request, response) {
+	r.table("sessions").filter({"type": "wpp"}).orderBy("name").run(DBConnection).then(function (data) {
 		response.send(data);
 	});
 });
-app.route("/sessions/science").get(function(request, response) {
-	r.table("sessions").filter({"type": "science"}).orderBy("name").run(DBConnection).then(function(data) {
+app.route("/sessions/science").get(function (request, response) {
+	r.table("sessions").filter({"type": "science"}).orderBy("name").run(DBConnection).then(function (data) {
 		response.send(data);
 	});
 });
 
 // 404 page
-app.use(function(request, response, next) {
+app.use(function (request, response, next) {
 	response.status(404).send("404 Not found!");
 });
 // Error handling
-app.use(function(err, request, response, next) {
+app.use(function (err, request, response, next) {
 	console.error(err);
 	response.status(500);
 	response.send("An error occurred!");
 });
 
+// Socket.io stuff
+io.on("connection", function (socket) {
+	// socket.emit("availability", {});
+	// socket.on("event", function (data) {
+	// 	console.log(data);
+	// });
+});
+
 var PORT = 80;
-app.listen(PORT, "0.0.0.0", function() {
+server.listen(PORT, "0.0.0.0", function () {
 	console.log("Server listening on port " + PORT);
 });
