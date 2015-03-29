4309da56d54b99143a0375f75c4c81c1be043623
Ryan Petschek <petschekr@gmail.com>
2015-03-29 13:57:06 -0400

    Switched to Orchestrate for the DB and finished panel registration

--- server.js
+++ server.js
@@ -4,28 +4,10 @@ var fs = require("fs");
 
 var moment = require("moment");
 var Promise = require("bluebird");
-//Promise.longStackTraces();
 fs = Promise.promisifyAll(fs);
-var r = require("rethinkdb");
-
+var Q = require("kew"); // Because the Orchesrate driver for Node.js forces its use
 // Initialize the database connection
-var DBConnection = undefined;
-function intializeDatabase() {
-	r.connect({"db": "wpp", "timeout": 20})
-		.then(function (connection) {
-			DBConnection = connection;
-			console.log("Connected to RethinkDB successfully");
-		})
-		.error(function (err) {
-			// Can't connect to the database
-			// Fatal error
-			throw err;
-		});
-}
-function destroyDatabase() {
-	DBConnection.close();
-}
-intializeDatabase();
+var db = require("orchestrate")("60e990e2-53e5-4be4-ba5c-5d4adf0cb6ca");
 
 // Set up the Express server
 var express = require("express");
@@ -46,7 +28,7 @@ app.use(cookieParser(
 	{
 		"path": "/",
 		"maxAge": 60 * 24 * 30 * 6, // 6 months
-		"secure": false, // Set this to true when you get a doman + SSL cert
+		"secure": false, // Set this to true when you get a domain + SSL cert
 		"httpOnly": true
 	}
 ));
@@ -67,9 +49,10 @@ app.route("/register").get(function (request, response) {
 app.route("/register/:code").get(function (request, response) {
 	var code = request.params.code.toString();
 	// Check if the provided code is valid
-	r.table("attendees").filter({"code": code}).count().run(DBConnection)
-		.then(function (count) {
-			if (count !== 1) {
+	db.search("users", `value.code: "${code}"`)
+		.then(function (results) {
+			results = results.body.results;
+			if (results.length !== 1) {
 				response.redirect("/register");
 				return;
 			}
@@ -77,31 +60,44 @@ app.route("/register/:code").get(function (request, response) {
 				.then(function (html) {
 					response.send(html);
 				})
-		})
+		});
 });
 // AJAX endpoints
 app.route("/info/:code").get(function (request, response) {
 	var code = request.params.code.toString();
-	r.table("attendees").filter({"code": code}).run(DBConnection).then(function (cursor) {
-		return cursor.next();
-	}).then(function (data) {
-		response.json({
-			"id": data.id,
-			"code": data.code,
-			"name": data.name,
-			"email": data.email
+	db.search("users", `value.code: "${code}"`)
+		.then(function (results) {
+			results = results.body.results;
+			if (results.length !== 1) {
+				response.status(404).json({
+					"error": "No user with that registration code exists"
+				});
+				return;
+			}
+			var result = results[0];
+			response.json({
+				"code": result.value.code,
+				"name": result.value.name,
+				"username": result.path.key
+			});	
 		});
-	}).error(function() {
-		response.status(404).json({
-			"error": "No user with that registration code exists"
-		});
-	});
 });
 app.route("/sessions/panels")
 	.get(function (request, response) {
-		r.table("panels").orderBy("name").run(DBConnection).then(function (data) {
-			response.send(data);
-		});
+		db.newSearchBuilder()
+			.collection("panels")
+			.limit(100)
+			.sort("name", "asc")
+			.query("*")
+			.then(function (results) {
+				results = results.body.results;
+				var panels = results.map(function (panelResponse) {
+					var panelObject = panelResponse.value;
+					panelObject.id = panelResponse.path.key;
+					return panelObject;
+				});
+				response.send(panels);
+			});
 	})
 	.post(postParser, function (request, response) {
 		// Register the user for their selected first choice
@@ -113,82 +109,110 @@ app.route("/sessions/panels")
 			});
 			return;
 		}
+		
 		function CancelError (message) {
 			this.message = message;
 		}
 		CancelError.prototype = Object.create(Error.prototype);
-		var sessionTimeKey;
-		// Confirm that this user exists
-		r.table("attendees").filter({"code": userID}).run(DBConnection)
-			.then(function (user) {
-				if (!user) {
-					return Promise.reject(new CancelError("Invalid user ID"));
+
+		var sessionKey = sessionID;
+		var attendeeKey = userID;
+		var sessionInfo = undefined;
+
+		db.search("users", `@path.key: ${attendeeKey}`)
+			.then(function (results) {
+				results = results.body.results;
+				if (results.length !== 1) {
+					return Q.reject(new CancelError("Invalid attendee ID."));
 				}
-				// Make sure that this is possible and increment the "taken field"
-				return r.table("panels").get(sessionID).run(DBConnection);
+				attendeeKey = results[0].path.key;
+				return db.search("panels", `@path.key: ${sessionKey}`)
 			})
-			.then(function (panel) {
-				if (!panel) {
-					return Promise.reject(new CancelError("Invalid session ID"));
+			.then(function (results) {
+				results = results.body.results;
+				if (results.length !== 1) {
+					return Q.reject(new CancelError("Invalid session ID."));
 				}
-				// Check if it has a valid time slot
-				switch (new Date(panel.time.start).valueOf()) {
-					case 1429707600000: // 9:00 AM - first session
-						sessionTimeKey = "first";
-						break;
-					case 1429710900000: // 9:55 AM - second session
-						sessionTimeKey = "second";
-						break;
-					case 1429716600000: // 11:30 AM - third session
-						sessionTimeKey = "third";
-						break;
-					case 1429719900000: // 12:25 PM - fourth session
-						sessionTimeKey = "fourth";
-						break;
-					case 1429723200000: // 1:20 PM - fifth session
-						sessionTimeKey = "fifth";
-						break;
-					default:
-						return Promise.reject(new CancelError("Invalid session time slot"));
-				}
-				if (panel.capacity.taken >= panel.capacity.total) {
-					// Too many people in this session
-					return Promise.reject(new CancelError("There are too many people in that panel. Please choose another."));
-				}
-				return r.table("panels").get(sessionID).update({
-					"capacity": {
-						"total": r.row("capacity")("total"),
-						"taken": r.row("capacity")("taken").add(1)
-					}
-				}).run(DBConnection);
+				sessionInfo = results[0].value;
+				return db.newGraphReader()
+					.get()
+					.from("panels", sessionKey)
+					.related("attendee");
 			})
-			.then(function () {
-				return r.table("panels").get(sessionID).run(DBConnection);
-			})
-			.then(function (panel) {
-				// Broadcast the new number of spots
+			.then(function (results) {
+				var currentAttendees = results.body.total_count || results.body.count;
+				if (currentAttendees >= sessionInfo.capacity.total) {
+					return Q.reject(new CancelError("There are too many people in that panel. Please choose another."));
+				}
+				// Broadcast the new number of attendees
 				io.emit("availability", {
 					"session": sessionID,
-					"taken": panel.capacity.taken
+					"taken": sessionInfo.capacity.taken + 1
 				});
-				// Put the session in the user's database entry
-				//return r.table("attendees").filter({"code": userID})
+				// Proceed with registration for this spot (create a bidirectional relationship)
+				var graphPromises = [];
+				graphPromises.push(db.newGraphBuilder()
+					.create()
+					.from("panels", sessionKey)
+					.related("attendee")
+					.to("users", attendeeKey));
+				graphPromises.push(db.newGraphBuilder()
+					.create()
+					.from("users", attendeeKey)
+					.related("attendee")
+					.to("panels", sessionKey));
+				return Q.all(graphPromises);
 			})
-			.catch(CancelError, function (err) {
-				response.send({
-					"error": err.message
+			.then(function () {
+				response.json({
+					"success": true
 				});
 			})
+			.fail(function (err) {
+				if (err instanceof CancelError) {
+					response.json({
+						"error": err.message
+					});
+				}
+				else {
+					console.error(err);
+					response.status(500).json({
+						"error": "An internal server error occurred."
+					});
+				}
+			});
 	});
 app.route("/sessions/wpp").get(function (request, response) {
-	r.table("sessions").filter({"type": "wpp"}).orderBy("name").run(DBConnection).then(function (data) {
-		response.send(data);
-	});
+	db.newSearchBuilder()
+		.collection("sessions")
+		.limit(100)
+		.sort("name", "asc")
+		.query('value.type: "wpp"')
+		.then(function (results) {
+			results = results.body.results;
+			var sessions = results.map(function (sessionResponse) {
+				var sessionObject = sessionResponse.value;
+				sessionObject.id = sessionResponse.path.key;
+				return sessionObject;
+			});
+			response.send(sessions);
+		})
 });
 app.route("/sessions/science").get(function (request, response) {
-	r.table("sessions").filter({"type": "science"}).orderBy("name").run(DBConnection).then(function (data) {
-		response.send(data);
-	});
+	db.newSearchBuilder()
+		.collection("sessions")
+		.limit(100)
+		.sort("name", "asc")
+		.query('value.type: "science"')
+		.then(function (results) {
+			results = results.body.results;
+			var sessions = results.map(function (sessionResponse) {
+				var sessionObject = sessionResponse.value;
+				sessionObject.id = sessionResponse.path.key;
+				return sessionObject;
+			});
+			response.send(sessions);
+		})
 });
 
 // 404 page
